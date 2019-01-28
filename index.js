0eea180a602d1a6406865e6f9496a65600338bdd
Hector Diaz <diazhector98@gmai.com>
2019-01-27 20:23:01 -0600

    Initial setup of menu with inquirer

--- index.js
+++ index.js
@@ -5,21 +5,131 @@ const inquirer = require('inquirer');
 
 const argv = require('yargs').argv;
 
-
-if(argv.trueo){
-    inquirer.prompt([
+const menuListObject =
+{
+    type: 'list',
+    name: 'menuOption',
+    choices: [
         {
-            name: 'extension',
-            message: 'Extension of files you want to move: ',
+            name: 'Move files to directory',
+            value: 'moveToDir'
         },
         {
-            name: 'directory',
-            message: 'Directory where you want it to move'
+            name: 'Delete files',
+            value: 'deleteFiles'
+        },
+        {
+            name: 'Create fileproof rule file',
+            value: 'initFproof'
+        },
+        {
+            name: 'Use fileproof rule file',
+            value: 'runFproof'
         }
-    ]).then(answers => {
-        console.log(answers.extension);
-    });
+    ]
 }
+
+const getExtension = (callback) => {
+    inquirer.prompt(
+        [
+            {
+                name: 'extension',
+                message: 'Extension (e.g jpg, pdf, docx): '
+            }
+        ]
+    ).then(answer => {
+        callback(answer.extension);
+    })
+}
+
+const getContaining = (callback) => {
+    inquirer.prompt(
+        [
+            {
+                name: 'containing',
+                message: 'Containing (e.g \'HW\', all files containg \'HW\'): '
+            }
+        ]
+    ).then(answer => {
+        callback(answer.containing);
+    })
+}
+
+const getDirectory = (callback) => {
+    inquirer.prompt(
+        [
+            {
+                name: 'directory',
+                message: 'Directory/Folder destiny: '
+            }
+        ]
+    ).then(answer => {
+        callback(answer.directory);
+    })
+}
+
+const handleMovingAttributes = (answer) => {
+    let ext;
+    let cont;
+    let dir;
+    if(answer.attributes.includes('ext')){
+        getExtension((extension) => {
+            ext = extension;
+            if(answer.attributes.includes('cont')){
+                getContaining((answer) => {
+                    cont = answer;
+                    getDirectory((directory) => {
+                        dir = directory;
+                        console.log(`Gonna move files with extension ${ext} and containing ${cont} to ${dir}`);
+                    });
+                });
+            } else {
+                getDirectory((directory) => {
+                    dir = directory;
+                    console.log(`Gonna move files with extension ${ext} to ${dir}`);
+                });
+            }
+        });
+    }
+
+}
+
+const startMovingToDirectory = () => {
+    inquirer.prompt(
+        [
+            {
+                type: 'checkbox',
+                name: 'attributes',
+                choices: 
+                [
+                    {
+                        name: 'Extension',
+                        value: 'ext'
+                    },
+                    {
+                        name: 'Containing',
+                        value: 'cont'
+                    }
+                ]
+            }
+        ]
+        ).then(answer => {
+            handleMovingAttributes(answer);
+        });
+}
+
+const handleMenuOption = (option) => {
+    if(option == 'moveToDir'){
+        startMovingToDirectory();
+    }
+}
+
+
+inquirer.prompt([menuListObject])
+.then(answers => {
+    handleMenuOption(answers.menuOption);
+});
+
 
 
 
